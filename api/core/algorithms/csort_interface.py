6ac440c0bedbefaf2d72cc8755364969b8850f2a
zimmangle <alexandrelb.700@gmail.com>
2023-11-16 00:06:45 -0300

    feat: callback functions

--- api/core/algorithms/csort_interface.py
+++ api/core/algorithms/csort_interface.py
@@ -1,5 +1,6 @@
 # csort_interface.py
-from ctypes import CDLL, c_int, POINTER
+from ctypes import CDLL, c_int, POINTER, CFUNCTYPE
+import json
 
 # Load the shared library (.so) or DLL
 csort_lib = CDLL('core/algorithms/lib/csort.so')  # Update with the correct path and library name
@@ -7,16 +8,19 @@ csort_lib = CDLL('core/algorithms/lib/csort.so')  # Update with the correct path
 # Define the function signature
 bubble_sort = csort_lib.bubble_sort
 selection_sort = csort_lib.selection_sort
-bubble_sort.argtypes = [POINTER(c_int), c_int]
-selection_sort.argtypes = [POINTER(c_int), c_int]
+bubble_sort.argtypes = [POINTER(c_int), c_int, CFUNCTYPE(None, POINTER(c_int))]
+selection_sort.argtypes = [POINTER(c_int), c_int, CFUNCTYPE(None, POINTER(c_int))]
 
-def csort(arr,method):
+def csort(arr, method, callback):
     size = len(arr)
     c_arr = (c_int * size)(*arr)
 
-    match method:
-        case 'b':
-            bubble_sort(c_arr, size)
-        case 's':
-            selection_sort(c_arr, size)
+    def c_callback(arr_ptr):
+        callback([arr_ptr[i] for i in range(size)])
+
+    if method == 'b':
+        bubble_sort(c_arr, size, CFUNCTYPE(None, POINTER(c_int))(c_callback))
+    elif method == 's':
+        selection_sort(c_arr, size, CFUNCTYPE(None, POINTER(c_int))(c_callback))
+
     return list(c_arr)
