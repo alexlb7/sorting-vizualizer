10fffc93bfd528a2f53036128cf5bca70f3c5b11
zimmangle <alexandrelb.700@gmail.com>
2023-11-16 14:42:52 -0300

    feat: requirements and base url

--- api/core/views.py
+++ api/core/views.py
@@ -4,13 +4,6 @@ import random
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
-def HomePage(request):
-    return render(request, 'home.html')
-
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
-
 def sorting_view(request):
 
     arr = [random.randint(5, 1000) for _ in range(100)]
